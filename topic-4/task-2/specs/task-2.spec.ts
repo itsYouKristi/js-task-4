f594738da72897a2e589964238665a3beef0a47d
Nikita Zernyukov <n.zernyukov@artsofte.ru>
2021-10-07 23:11:51 +0500

    Fix

--- topic-4/task-2/specs/task-2.spec.ts
+++ topic-4/task-2/specs/task-2.spec.ts
@@ -1,24 +1,31 @@
 const { Hamburger } = require('../index');
 
-const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
+
 
 test('Method works fine', () => {
-    expect(hamburger.calculateCalories()).toEqual(45),
+    const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+    hamburger.addTopping(Hamburger.TOPPING_MAYO);
+    expect(hamburger.calculateCalories()).toEqual(45)
     expect(hamburger.calculatePrice()).toEqual(80)
 });
 
-const newHamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
-newHamburger.addTopping(Hamburger.TOPPING_MAYO);
-newHamburger.addTopping(Hamburger.TOPPING_SPICE);
+
 
 test('Recalculate is works', () => {
-    expect(newHamburger.calculatePrice()).toEqual(95),
+    const newHamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+    newHamburger.addTopping(Hamburger.TOPPING_MAYO);
+    newHamburger.addTopping(Hamburger.TOPPING_SPICE);
+    expect(newHamburger.calculatePrice()).toEqual(95)
     expect(newHamburger.getSize() === Hamburger.SIZE_LARGE).toEqual(false)
 })
 
-newHamburger.removeTopping(Hamburger.TOPPING_SPICE);
+
 
 test('Remove something', () => {
+    const newHamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+    newHamburger.addTopping(Hamburger.TOPPING_MAYO);
+    newHamburger.addTopping(Hamburger.TOPPING_SPICE);
+    expect(newHamburger.getToppings().length).toEqual(2)
+    newHamburger.removeTopping(Hamburger.TOPPING_SPICE);
     expect(newHamburger.getToppings().length).toEqual(1);
 })
--- topic-4/task-3/specs/task-3.spec.ts
+++ topic-4/task-3/specs/task-3.spec.ts
@@ -3,5 +3,5 @@ const { getSpeedy, getLazy } = require('../index');
 getSpeedy().eat("apple");
 
 test('Method works fine', () => {
-    expect(getLazy().stomach).toBe([]);
+    expect(getLazy().stomach.length).toBe(0);
 });
